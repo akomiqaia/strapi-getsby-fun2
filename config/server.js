d0d7cb0390f3de928950fb41f69e13bfd7d86d1e
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-05 13:19:01 +0100

    initiliase strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7461b256529fb5a627b0fccfa2bbd172'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6dadc3d8-98b8-4aa1-854a-1f77f7f90cef'
+};
