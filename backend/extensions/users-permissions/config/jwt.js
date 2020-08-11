5de7c97d05e812997c5e6e33042a6add2c538c92
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-08-11 10:54:44 +0100

    initliase new strapi and gatsby project

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '10260eab-596a-47e0-9510-21e7cfc9636f'
+};
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '6dadc3d8-98b8-4aa1-854a-1f77f7f90cef'
-};
